a9a1edea621ac70ea02c2211bed026c2c81ae114
Stas Y <yankevych.stas@gmail.com>
2023-05-20 20:13:40 +0300

    feat: add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+import http from 'http';
+import { Server } from 'socket.io';
+import express, { Express, Request, Response } from 'express';
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {
