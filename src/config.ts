faa78a2809e7673230ec64373b8ee7bb14bbd66a
auvy <datruegvoltage@gmail.com>
2023-05-26 04:31:47 +0300

    simplified some values

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,23 @@
 export const MAP_W = 500
 export const MAP_H = 200
 
+export const PLANET_DISTANCE_DELTA = 9
+
 export const SPEED = 500
 
-export const PLANET_D_MAX_DIVISOR = 3
-export const PLANET_D_MIN_DIVISOR = 24
-
-export const PLANET_STARTER_D      = 2
-export const PLANET_STARTER_HEALTH = 3 
+export const PLAYER_COUNT = 2
 
 export const ATTEMPTS = 1000
 
-export const MAX_HEALTH = 50
-export const MIN_HEALTH = 10
 
-// const PLANET_DISTANCE_MIN = 
+export const RADIUS_STARTER = 9
+export const HEALTH_STARTER = 10
+
+export const HEALTH_MIN = 5
+export const HEALTH_MAX = 50
+
+export const RADIUS_MIN = 3
+export const RADIUS_MAX = 24
 
 export const PLANET_N_MIN = 6
 export const PLANET_N_MAX = 14
-
-export const PLAYER_COUNT = 2
