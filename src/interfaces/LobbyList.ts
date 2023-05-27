4e6fa14eaec919e53dbec4c73e7f4f17342ed433
Stas Y <yankevych.stas@gmail.com>
2023-05-27 14:52:57 +0300

    feat: add lobby list

---
+++ src/interfaces/LobbyList.ts
@@ -0,0 +1,4 @@
+export interface LobbyList {
+  id: string,
+  usersAmount: number
+}
--- src/sockets.ts
+++ src/sockets.ts
@@ -1,26 +1,29 @@
 import { Server, Socket } from 'socket.io';
 import Storage from './entities/Storage';
-import {IncomingEvents, OutcomingEvents, GameParticipants, SendUnitsDetails} from './interfaces';
+import { IncomingEvents, OutcomingEvents, GameParticipants, SendUnitsDetails, LobbyList } from './interfaces';
+import { User } from './interfaces/User';
 
 export function handleSocketConnection(io: Server, socket: Socket, storage: Storage) {
   console.log(`${socket.id} connected`);
 
-  socket.on(IncomingEvents.CREATE_NEW_GAME, (callback: (gameId: string) => void) => {
-    const gameId = storage.createNewGame(socket.id);
+  socket.on(IncomingEvents.CREATE_NEW_GAME, (user: User, callback: (gameId: string) => void) => {
+    const gameId = storage.createNewGame(user);
+    console.log(`send game`, gameId)
     socket.join(gameId);
     callback(gameId);
   });
 
-  socket.on(IncomingEvents.JOIN_GAME, (gameId: string, callback: (game: GameParticipants) => void) => {
+  socket.on(IncomingEvents.JOIN_GAME, (user: User, gameId: string, callback: (game: GameParticipants) => void) => {
     const game = storage.getGame(gameId)
 
     if (game) {
-      game.joinGame(socket.id);
+      console.log(`join to game`, game)
+      game.joinGame(user);
 
       io.to(gameId).emit(OutcomingEvents.PLAYER_JOINED, socket.id)
       socket.join(gameId);
 
-      callback({ gameId: game.id, player1: game.player1, player2: game.player2 });
+      callback({ gameId: game.id, players: game.players });
     }
   });
 
@@ -28,6 +31,7 @@ export function handleSocketConnection(io: Server, socket: Socket, storage: Stor
     const game = storage.getGame(gameId)
 
     if (game) {
+      console.log(`start game`, game)
       game.startGame();
       io.to(gameId).emit(OutcomingEvents.GAME_STARTED, game.getGameDetails())
     }
@@ -52,5 +56,11 @@ export function handleSocketConnection(io: Server, socket: Socket, storage: Stor
         console.log(err);
       }
     }
+  })
+
+  socket.on(IncomingEvents.GET_LOBBY_LIST, (callback: (games: LobbyList[]) => void) => {
+    const games = storage.games
+    const lobbyList = games.map((game) => ({ id: game.id, usersAmount: game.players.length }))
+    callback(lobbyList);
   })
 }
